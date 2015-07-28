548f9ccae663d6c93a083d2a9609708d478de549
Legion <ddnymlil@gmail.com>
2015-07-27 19:47:32 -0700

    Post text is being read from the database, this completes all the necessary database access

--- classic.py
+++ classic.py
@@ -28,7 +28,8 @@ class Application(tornado.web.Application):
 			(r"/count", CountHandler),
 			(r"/", tornado.web.RedirectHandler, dict(url=r"/main")),
 			(r"/([a-z]+)", DatabaseHandler),
-			(r"/([a-z]+)/([0-9]+)",ForumHandler), 
+			(r"/([a-z]+)/([0-9]+)",ForumHandler),
+			(r"/([a-z]+)/([0-9]+)/([0-9]+)",ThreadHandler), 
 		]
 		settings = dict(
 			template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -88,15 +89,38 @@ class ForumHandler(HandlerBase):
 			out = {}
 			thread_list = []
 			c = self.application.db.cursor()
-			select_table = "SELECT forum_id, topic_title, topic_id FROM %s" % prefix + "_topics"
+			select_table = "SELECT forum_id, topic_title, topic_id, topic_poster FROM %s" % prefix + "_topics"
 			select_text = select_table + " WHERE forum_id=%s ORDER BY topic_id"
 			c.execute(select_text, (forum_id,))
 			for row in c.fetchall():
 				title = row[1].decode('latin1').encode('utf8')
-				thread_list.append({"title":title, "url":root_url+db+"/"+forum_id+"/"+str(row[2])})
+				thread_list.append({"title":title, "creator_id":str(row[2]),  "url":root_url+db+"/"+forum_id+"/"+str(row[2])})
 			out["threads"] = thread_list
 			self.write(out)
 
+
+#TODO: use a JOIN!
+class ThreadHandler(HandlerBase):
+	def get(self, db, forum_id, topic_id):
+		prefix = self.prefix(db)
+		if prefix:
+			out = {}
+			post_list = []
+			c = self.application.db.cursor()
+			select_table = "SELECT post_id, poster_id from %s" % prefix + "_posts"
+			select_text = select_table + " WHERE topic_id=%s ORDER BY post_id"
+			c.execute(select_text, (topic_id,))
+			for row in c.fetchall():
+				c2 = self.application.db.cursor()
+				select_table2 = "SELECT post_subject,post_text from %s" % prefix + "_posts_text"
+				select_text2 = select_table2 + " WHERE post_id=%s"
+				c2.execute(select_text2, (row[0],))
+				row2 = c2.fetchone()
+				subject = row2[0].decode('latin1').encode('utf8')
+				text = row2[1].decode('latin1').encode('utf8')
+				post_list.append({"subject":subject, "text":text,"poster_id":str(row[1]), "url":root_url+db+"/"+forum_id+"/"+topic_id+"/"+str(row[0])})
+			out["posts"] = post_list
+			self.write(out) 
 
 
 
